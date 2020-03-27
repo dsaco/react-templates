8b63d3eb027c776fbe3754bea7436f1ff8c17026
dsaco <2363140194@qq.com>
2020-03-27 11:53:00 +0800

    basic commit

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const baseConfig = require('./webpack.config.base');
+
+module.exports = merge(baseConfig, {
+    mode: 'development',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: [
+                                require('autoprefixer'),
+                            ],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(gif|jpg|png)$/,
+                use: 'url-loader',
+            },
+        ],
+    },
+    devServer: {
+        hot: true,
+        port: 3000,
+        open: true,
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+});
---
+++ build/webpack.config.prod.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+const merge = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const baseConfig = require('./webpack.config.base');
+
+module.exports = merge(baseConfig, {
+    mode: 'production',
+    output: {
+        filename: 'scripts/[name].[chunkhash:8].js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: [
+                                require('autoprefixer'),
+                            ],
+                        },
+                    },
+                ],
+            },
+            {
+                test:/\.(gif|jpg|png)$/,
+                use:'file-loader?name=images/[name].[ext]',
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin(),
+            new OptimizeCSSAssetsPlugin({}),
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].[chunkhash:8].css',
+        }),
+    ],
+});

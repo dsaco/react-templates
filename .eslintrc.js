934d56a2c42a13fbe2d401bb9b07b4abd56d2291
dsaco <2363140194@qq.com>
2020-03-29 00:08:52 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,4 +16,10 @@ module.exports = {
             version: '16'
         }
     },
+    rules: {
+        // 关闭 禁用未声明的变量
+        'no-undef': 0,
+        // 关闭 检测props数据类型
+        'react/prop-types': 0,
+    }
 };

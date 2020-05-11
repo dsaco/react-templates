67b0e46926016b8ae1251337427df8098d3e7c00
dsaco <2363140194@qq.com>
2020-05-11 22:20:08 +0800

    add prettier

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,39 +4,35 @@ const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 
 module.exports = merge(commonConfig, {
-    mode: 'development',
-    devtool: 'cheap-module-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            ident: 'postcss',
-                            plugins: [
-                                require('autoprefixer'),
-                            ],
-                        },
-                    },
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(gif|jpg|png|svg)$/,
-                use: 'url-loader',
-            },
-        ],
-    },
-    devServer: {
-        hot: true,
-        port: 3000,
-        open: true,
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-    ],
+	mode: 'development',
+	devtool: 'cheap-module-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							ident: 'postcss',
+							plugins: [require('autoprefixer')],
+						},
+					},
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(gif|jpg|png|svg)$/,
+				use: 'url-loader',
+			},
+		],
+	},
+	devServer: {
+		hot: true,
+		port: 3000,
+		open: true,
+	},
+	plugins: [new webpack.HotModuleReplacementPlugin()],
 });
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,55 +3,50 @@ const path = require('path');
 const merge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const commonConfig = require('./webpack.common');
 
 module.exports = merge(commonConfig, {
-    mode: 'production',
-    devtool: 'source-map',
-    output: {
-        filename: 'scripts/[name].[chunkhash:8].js',
-        path: path.resolve(__dirname, '../dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s?css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            ident: 'postcss',
-                            plugins: [
-                                require('autoprefixer'),
-                            ],
-                        },
-                    },
-                    'sass-loader',
-                ],
-            },
-            {
-                test:/\.(gif|jpg|png|svg)$/,
-                use:'file-loader?name=images/[name].[ext]',
-            },
-        ],
-    },
-    optimization: {
-        minimizer: [
-            new TerserPlugin(),
-            new OptimizeCSSAssetsPlugin({}),
-        ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'styles/[name].[chunkhash:8].css',
-        }),
-        new CleanWebpackPlugin(),
-    ],
+	mode: 'production',
+	devtool: 'source-map',
+	output: {
+		filename: 'scripts/[name].[chunkhash:8].js',
+		path: path.resolve(__dirname, '../dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							ident: 'postcss',
+							plugins: [require('autoprefixer')],
+						},
+					},
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(gif|jpg|png|svg)$/,
+				use: 'file-loader?name=images/[name].[ext]',
+			},
+		],
+	},
+	optimization: {
+		minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'styles/[name].[chunkhash:8].css',
+		}),
+		new CleanWebpackPlugin(),
+	],
 });
